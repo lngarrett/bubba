544675d5e618d691ef7414375c481915c5e370ba
Logan Garrett <logan@Logans-MacBook-Pro.local>
2015-06-23 23:35:32 -0400

    thread

--- app.rb
+++ app.rb
@@ -9,25 +9,37 @@ set :server, 'thin'
 $redis = Redis.new
 $config = YAML.load_file('config.yaml')["config"]
 
+$count = 0
+
+Thread.new do # Background tasks
+  #seedRedis
+  #fullCredit
+  while true do
+    sleep $config["credit"]["interval"]
+    #incrCredit
+    $count += 1
+  end
+end
+
+get '/' do
+  "Testing background work thread: sum is #{$count}"
+end
+
+#get '/' do
+#  $put = $redis.hget("cameras:frontcam", :name) + " " + $redis.hget("cameras:frontcam", :credit) + " " + $count.to_s + " " + $config["credit"]["interval"].to_s
+#  "#{$put}"
+#end
+
+get '/:camera/motion' do
+  camera = params[:camera]
+  alertMotion(camera)
+end
+
 def pushover(message)
   uri = URI.encode("https://api.pushover.net/1/messages.json")
   appKey = $redis.get "config:pushover:app-key"
   userKey = $redis.get "config:pushover:user-key"
   RestClient.post uri, { :token => appKey, :user => userKey, :message => message }
-end
-
-get '/:camera/motion' do
-camera = params[:camera]
-alertMotion(camera)
-end
-
-Thread.new do # Background tasks
-  seedRedis
-  fullCredit
-  while true do
-     sleep $config["credit"]["interval"]
-     incrCredit
-  end
 end
 
 def seedRedis
@@ -57,6 +69,15 @@ def incrCredit
   end
 end
 
+def decrCredit(camera)
+  credit = camera["credit"]
+  $redis.hincrby("cameras:#{camera}", :credit, -1) unless credit == 0
+end
+
 def alertMotion(camera)
-  pushover("#{camera} alert")
+  credit = $redis.hget("cameras:#{camera}", :credit)
+  if credit.to_i > 0 
+    pushover("#{camera} alert #{credit}")
+    decrCredit(camera)
+  end
 end
